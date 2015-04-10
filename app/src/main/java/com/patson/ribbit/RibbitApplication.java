f03e149abd0dff3d45a6a736d6bb26ee69b17050
pat-son <patrick.son@comcast.net>
2015-04-10 19:20:53 -0400

    Updated SignUpActivity

--- app/src/main/java/com/patson/ribbit/RibbitApplication.java
+++ app/src/main/java/com/patson/ribbit/RibbitApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.util.Log;
 
 import com.parse.Parse;
-import com.parse.ParseObject;
 
 /**
  * Created by patrick on 4/8/15.
@@ -17,10 +16,6 @@ public class RibbitApplication extends Application {
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, "d4Dp0rTy8bfJj1GNfptaL5VfhICZr6e5fEzDLXFl", "1z39pblMeMXjRcQ1AUnZYu8sHUDcpXaI6wBg5J0I");
         Log.d("RibbitApplication", "Success?");
-
-        ParseObject testObject = new ParseObject("TestObject");
-        testObject.put("foooo", "bar");
-        testObject.saveInBackground();
 
     }
 
